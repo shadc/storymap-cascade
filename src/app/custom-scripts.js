7637fd1a635fcc27d5de33e9e2f94f7befe26a59
ADMIN\shadc <shadc@gmx.com>
2019-01-29 21:15:59 -0800

    updated custom-scripts.js

--- src/app/custom-scripts.js
+++ src/app/custom-scripts.js
@@ -1,63 +1,30 @@
-require(['dojo/topic'], function(topic) {
+require(['dojo/topic'], function (topic) {
   /*
    * Custom Javascript to be executed while the application is initializing goes here
    */
 
   // The application is ready
-  topic.subscribe('tpl-ready', function() {
+  topic.subscribe('tpl-ready', function () {
     /*
      * Custom Javascript to be executed when the application is ready goes here
      */
-    function scrollToAnchor(){
-        let section = $(".section-layout-title:eq(" + currentSection + ")");
-        $('html,body').animate({scrollTop: section.offset().top},'slow');
-      }
-      
-      // var i = -1; 
-      
-      // $(document).keydown(function (e) {
+    function scrollToAnchor() {
+      let section = $(".section-layout-title:eq(" + currentSection + ")");
+      $('html,body').animate({ scrollTop: section.offset().top - 50 }, 'slow');
+    }
 
-      //   if (e.keyCode == 37 || e.keyCode == 38) {
-      //         if (i == 0) return;
-      //         i--;
-      //         scrollToAnchor();
-      //  } else if (e.keyCode == 39 || e.keyCode == 40) {   
+    let currentSection = -1, maxSections = $(".section-layout-title").length - 1;
 
-      //         i++;
-      //         event.preventDefault();
-      //         scrollToAnchor();
-      //     }
-      // });
-
-
-
-
-
-
-     let currentSection = -1, maxSections = $(".section-layout-title").length -1;
-
-     //alert(maxSections);
-    // //$(".section-layout-title").css('color', 'green');
-
-      $(document).keydown(function(e) {
-         if ((e.keyCode == 37 || e.keyCode == 38) ) {  //Left or up 
-            if (currentSection > 0) currentSection--;
-            //e.preventDefault();
-            scrollToAnchor();
-
-         }
-
-       if (e.keyCode == 39 || e.keyCode == 40) {  //right or down 
-        if (currentSection < maxSections) currentSection++;
-        //e.preventDefault();
+    $(document).keydown(function (e) {
+      if ((e.keyCode == 37 || e.keyCode == 38)) {  //Left or up 
+        if (currentSection > 0) currentSection--;
         scrollToAnchor();
-
-       }
-
-    //    $(".section-layout-title").each(function( index ) {
-    //      alert( index );
-    //    });
-      });
+      }
+      if (e.keyCode == 39 || e.keyCode == 40) {  //right or down 
+        if (currentSection < maxSections) currentSection++;
+        scrollToAnchor();
+      }
+    });
 
     //console.log('Cascade is ready');
   });
@@ -65,8 +32,8 @@ require(['dojo/topic'], function(topic) {
   /*
    * Custom Javascript to be executed when a section becomes active
    */
-  topic.subscribe('story-navigated-section', function(/*cfg*/) {
-     //console.log('The section', cfg.index, 'is now active');
+  topic.subscribe('story-navigated-section', function (/*cfg*/) {
+    //console.log('The section', cfg.index, 'is now active');
   });
 
 });
